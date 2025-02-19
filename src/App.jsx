006dac8fe578c570e27323f9030871da5335b0fd
sofia-achilleos <sofia.achilleos.holst.hansen@gmail.com>
2025-02-19 14:05:40 +0100

    somthing

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <p>hej</p>
+      <p>hej</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
