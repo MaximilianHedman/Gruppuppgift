27d0b8286b4f8c37f27603eeab81f0eb1185b82c
MaximilianHedman <maximilian.hedman@chasacademy.se>
2025-02-26 15:29:03 +0100

    Changed ShoppingCartModal and FavoriteListModal to pages and their name, also added fontawesome link in index.html

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import WomensClothing from "./pages/WomensClothing/WomensClothing";
 import MensClothing from "./pages/MensClothing/MensClothing";
 import Jewelery from "./pages/Jewelery/Jewelery";
 import ProductDetails from "./components/ProductDetails/ProductDetails";
+import ShoppingCart from "./pages/ShoppingCart/ShoppingCart";
+import Favorites from "./pages/Favorites/Favorites";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
 import Footer from "./components/Footer/Footer";
@@ -21,6 +23,8 @@ const App = () => {
                 <Route path='/mens-clothing' element={<MensClothing />} />
                 <Route path='/jewelery' element={<Jewelery />} />
                 <Route path='/product/:id' element={<ProductDetails />} />
+                <Route path='/shoppingcart' element={<ShoppingCart />} />
+                <Route path='/favorites' element={<Favorites />} />
                 <Route path='/about' element={<About />} />
                 <Route path='/contact' element={<Contact />} />
             </Routes>
