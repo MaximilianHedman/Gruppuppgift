e917aa5fb937590f4c30698df21d1019481d0567
Nikita Quazi <nikita.quazi@gmail.com>
2025-02-26 16:16:29 +0100

    no changes

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import './ProductCard.css';
+import React from "react";
+import "./ProductCard.css";
 
 const ProductCard = ({ product, onProductClick }) => {
-    return (
-        <div className="product-card" onClick={() => onProductClick(product.id)}>
-            <img src={product.image} alt={product.title} />
-            <div className="product-info">
-                <p>{product.title}</p>
-            </div>
-            <p>${product.price}</p>
-        </div>
-    );
+  return (
+    <div className="product-card" onClick={() => onProductClick(product.id)}>
+      <img src={product.image} alt={product.title} />
+      <div className="product-info">
+        <p>{product.title}</p>
+      </div>
+      <p>${product.price}</p>
+    </div>
+  );
 };
 
 export default ProductCard;
