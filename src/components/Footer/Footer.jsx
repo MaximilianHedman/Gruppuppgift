d795c97fbc6a27c65d66ca832205a8aa98aca250
awiwa98 <awiwawidmark123@icloud.com>
2025-02-27 22:50:33 +0100

    börjat me footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,57 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+    <footer className="footer-container">
+    <div className="quick-links"> 
+      <ul>
+        <h3>SUPPORT</h3>
+        <li>Support</li>
+        <li>Contact</li>
+      </ul>
+
+      <ul>
+        <h3>ABOUT</h3>
+        <li>About us</li>
+        <li>FAQ</li>
+      </ul>
+
+      <ul>
+        <h3>SHOP</h3>
+        <li>Women</li>
+        <li>Men</li>
+        <li>Jewelery</li>
+      </ul>
+
+    </div>
+
+    <div className="subscribe-form"> 
+      <h2>GET UPDATES</h2>
+      <p>Stay updated with our lates news, <br />
+        exlusive offers, and insights!
+      </p>
+
+      <div className="subscribe-input">  
+      <input type="email" placeholder="Enter your email.." className="subscribe-field" />
+      <button className="subscribe-button">SUBSCRIBE</button>
+      </div>
+    </div>
+
+    <div className="socials">
+      <h2>CONNECT WITH US</h2>
+
+    <div className="socials-icons">
+
+      <i className="fa-brands fa-instagram"></i>
+      <i className="fa-brands fa-facebook-f"></i>
+      <i className="fa-brands fa-twitter"></i>
+      <i className="fa-brands fa-tiktok"></i>
+      </div>
+
+    </div>
+      
+    </footer>
+    </>
   )
 }
 
